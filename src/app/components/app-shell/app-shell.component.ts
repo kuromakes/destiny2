d3900ebdfbaca3ab265a43b89e8f1eddbd1dfd16
Jesse Rogers <jesse@jesserogers.net>
2019-12-08 12:05:21 -0800

    app shell progress

--- src/app/components/app-shell/app-shell.component.ts
+++ src/app/components/app-shell/app-shell.component.ts
@@ -3,6 +3,7 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
 @Component({
   selector: 'app-shell',
   templateUrl: './app-shell.component.html',
+  styleUrls: ['./app-shell.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppShellComponent {
