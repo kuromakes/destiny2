8bb2aa108ae23ff3acd7bf8b41813a1c7ea90c97
Jesse Rogers <jesse@jesserogers.net>
2019-12-08 12:07:08 -0800

    added public api for @components

---
+++ src/app/components/public-api.ts
@@ -0,0 +1,7 @@
+export * from './backdrop/backdrop.component';
+// hero
+export * from './hero/hero.component';
+export * from './hero/hero.component.module';
+// logo
+export * from './logo/logo.component';
+export * from './logo/logo.module';
