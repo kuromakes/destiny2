48b93659163550953e369f0973242f9080ad2202
Jesse Rogers <jesse@jesserogers.net>
2019-12-08 20:25:48 -0800

    added clan search to home page

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { BungieService, SEOService } from '@service';
+import { take } from 'rxjs/operators';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-home-page',
@@ -7,5 +10,32 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HomePageComponent {
+
+  public clanName: string;
+
+  public searchResults = new BehaviorSubject<any[]>(null);
+
+  constructor(private bungie: BungieService, private seo: SEOService) {
+    this.seo.resetTitle();
+    this.seo.updateDescription();
+  }
+
+  public search(): void {
+    this.bungie.searchClans({
+      name: this.clanName.trim(),
+      groupType: 1
+    }).pipe(
+      take(1)
+    ).subscribe(
+      clans => {
+        if (clans && clans.results) {
+          this.searchResults.next(clans.results);
+        }
+      },
+      err => {
+        console.error('Unexpected failure retrieving clan search results');
+      }
+    );
+  }
 
 }
--- src/app/views/home/home.module.ts
+++ src/app/views/home/home.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './home.component';
 import { HeroComponentModule, InputComponentModule } from '@components';
+import { MaterialModule } from 'src/app/material.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
     CommonModule,
     HeroComponentModule,
-    InputComponentModule
+    InputComponentModule,
+    MaterialModule,
+    RouterModule
   ],
   declarations: [
     HomePageComponent
