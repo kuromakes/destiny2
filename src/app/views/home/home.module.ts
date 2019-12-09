b6be781c598e72ea88617c2bc690002253a29a71
Jesse Rogers <jesse@jesserogers.net>
2019-12-08 16:51:38 -0800

    update homepage module

--- src/app/views/home/home.module.ts
+++ src/app/views/home/home.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './home.component';
-import { HomePageRoutingModule } from './home.routing.module';
+import { HeroComponentModule, InputComponentModule } from '@components';
 
 @NgModule({
-  imports: [ CommonModule, HomePageRoutingModule ],
-  declarations: [ HomePageComponent ]
+  imports: [
+    CommonModule,
+    HeroComponentModule,
+    InputComponentModule
+  ],
+  declarations: [
+    HomePageComponent
+  ]
 })
 export class HomePageModule {
 
