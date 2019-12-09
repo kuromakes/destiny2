4d3fdbb6444232b48f3c078c4d99ad993437daf4
Jesse Rogers <jesse@jesserogers.net>
2019-12-08 16:49:30 -0800

    updated component public api

--- src/app/components/public-api.ts
+++ src/app/components/public-api.ts
@@ -1,7 +1,11 @@
 export * from './backdrop/backdrop.component';
+export * from './backdrop/backdrop.module';
 // hero
 export * from './hero/hero.component';
 export * from './hero/hero.component.module';
 // logo
 export * from './logo/logo.component';
 export * from './logo/logo.module';
+// input
+export * from './input/input.component';
+export * from './input/input.module';
